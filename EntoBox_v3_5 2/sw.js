d5891c218d4e8e32035ce0109a82cd3d04c20555
Sanny Sanny <94448806+SaniyaSani@users.noreply.github.com>
2026-08-01 23:08:46 +0200

    Delete EntoBox_v3_5 2 directory

--- EntoBox_v3_5 2/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='entobox-v3-dashboard-5';
-const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon.svg','./assets/demo-box.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;}).catch(()=>caches.match('./index.html'))));});
